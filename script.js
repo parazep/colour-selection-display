fc91536ec6f0681432a0f930693ef1d32f780925
Pete Reid <parazep@gmail.com>
2017-09-17 18:56:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(".box").click(function() {
   $(".box").removeClass("selected");
   $(this).addClass("selected");
   var boxColour = $(this).css("background-color");
-  console.log(boxColour);
+  console.log(boxColour); //For Testing Purposes
   $("#result").text(boxColour);
 });
 
